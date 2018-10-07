c97bf6928075a47e2b03f5d6161778beae718d08
TomohiroAkutsu <atom4lightning@gmail.com>
2018-10-08 00:33:54 +0900

    first commit

---
+++ lib/model/user.dart
@@ -0,0 +1,24 @@
+import 'package:aqueduct/managed_auth.dart';
+import 'package:heroes/heroes.dart';
+import 'package:heroes/model/hero.dart';
+
+class User extends ManagedObject<_User> implements _User, ManagedAuthResourceOwner<_User> {
+  @Serialize(input: true, output: false)
+  String password;
+}
+
+class _User extends ResourceOwnerTableDefinition {
+  @primaryKey
+  int id;
+
+  @Column(unique: true, indexed: true)
+  String username;
+
+  @Column(omitByDefault: true)
+  String hashedPassword;
+
+  @Column(omitByDefault: true)
+  String salt;
+
+  ManagedSet<ManagedAuthToken> tokens;
+}
